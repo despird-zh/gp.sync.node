f055c81c49a75dc5e74cbdcc69ce8c7cc0d40d4e
gdiao <gary.diao@yahoo.com>
2017-09-05 17:20:33 +0800

    merge InfoIds methods into IdKeys

--- src/main/java/com/gp/sync/SyncNodeEngine.java
+++ src/main/java/com/gp/sync/SyncNodeEngine.java
@@ -1,6 +1,41 @@
 package com.gp.sync;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import com.gp.core.AppContextHelper;
+import com.gp.sync.message.SyncPullMessage;
+import com.gp.sync.message.SyncPushMessage;
+
+/**
+ * Wrap all the business logic, 
+ * 
+ * @author gdiao
+ * @version 0.1 2017-5-6
+ **/
 public class SyncNodeEngine {
-
+	
+	static Logger LOGGER = LoggerFactory.getLogger(SyncNodeEngine.class);
+	
+	private static SyncNodeEngine instance;
+	
+	
+	
+	public static SyncNodeEngine getInstance() {
+		
+		if( instance == null) {
+			instance = new SyncNodeEngine();
+			AppContextHelper.autowireBean(instance);
+		}
+		
+		return instance;
+	}
+	
+	public void handlePushMessage(SyncPushMessage pushMessage) {
+		
+	}
+	
+	public void handlePullMessage(SyncPullMessage pullMessage) {
+		
+	}
 }
